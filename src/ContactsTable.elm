434ebe61260676d3dff6a5c71657d86227b4a253
Bill Peregoy <bill.peregoy@endurance.com>
2016-12-28 22:41:39 -0500

    Split out ContactsTable

---
+++ src/ContactsTable.elm
@@ -0,0 +1,101 @@
+module ContactsTable exposing (view)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Model exposing (..)
+import Contact exposing (..)
+import HttpUtils exposing (..)
+
+
+headerInfoRow : Model -> Html Msg
+headerInfoRow model =
+    let
+        infoLine =
+            if (List.length model.selectedContacts) == 0 then
+                text
+                    ((model.contactsCount |> toString)
+                        ++ " Contacts. Select contacts to organize, export or remove... "
+                    )
+            else
+                text
+                    ((model.selectedContacts |> List.length |> toString) ++ " Selected")
+
+        manageMenu =
+            if (List.length model.selectedContacts) == 0 then
+                span [] []
+            else
+                span
+                    [ style
+                        [ ( "margin-left"
+                          , "5px"
+                          )
+                        ]
+                    ]
+                    [ a [ href "#", onClickNoDefault ShowAddToListsModal ] [ text "Add to Lists" ] ]
+    in
+        tr []
+            [ th [ colspan 4 ]
+                [ span []
+                    [ infoLine
+                    , manageMenu
+                    ]
+                ]
+            ]
+
+
+tableHeader : Model -> Html Msg
+tableHeader model =
+    let
+        ids =
+            List.map .id model.contacts
+    in
+        thead []
+            [ headerInfoRow model
+            , tr []
+                [ th []
+                    [ input
+                        [ type_ "checkbox"
+                          --, onCheck (SetCheckbox ids)
+                        ]
+                        []
+                    ]
+                , th [] [ text "Last Name" ]
+                , th [] [ text "First Name" ]
+                , th [] [ text "Email" ]
+                ]
+            ]
+
+
+tableBody : List Contact -> Html Msg
+tableBody contacts =
+    tbody []
+        (contactRows contacts)
+
+
+view : Model -> Html Msg
+view model =
+    table [ class "table table-striped" ]
+        [ (tableHeader model)
+        , tableBody model.contacts
+        ]
+
+
+contactRows : List Contact -> List (Html Msg)
+contactRows contacts =
+    List.map
+        (\contact ->
+            tr []
+                [ td []
+                    [ input
+                        [ type_ "checkbox"
+                        , onCheck (SetCheckbox contact.id)
+                        ]
+                        []
+                    ]
+                , td [] [ text (Maybe.withDefault "" contact.lastName) ]
+                , td [] [ text (Maybe.withDefault "" contact.firstName) ]
+                , td [] [ text contact.email.address ]
+                ]
+        )
+        contacts
--- src/View.elm
+++ src/View.elm
@@ -13,99 +13,7 @@ import Http
 import HttpUtils exposing (..)
 import AddToListModal
 import RenameModal
-
-
-headerInfoRow : Model -> Html Msg
-headerInfoRow model =
-    let
-        infoLine =
-            if (List.length model.selectedContacts) == 0 then
-                text
-                    ((model.contactsCount |> toString)
-                        ++ " Contacts. Select contacts to organize, export or remove... "
-                    )
-            else
-                text
-                    ((model.selectedContacts |> List.length |> toString) ++ " Selected")
-
-        manageMenu =
-            if (List.length model.selectedContacts) == 0 then
-                span [] []
-            else
-                span
-                    [ style
-                        [ ( "margin-left"
-                          , "5px"
-                          )
-                        ]
-                    ]
-                    [ a [ href "#", onClickNoDefault ShowAddToListsModal ] [ text "Add to Lists" ] ]
-    in
-        tr []
-            [ th [ colspan 4 ]
-                [ span []
-                    [ infoLine
-                    , manageMenu
-                    ]
-                ]
-            ]
-
-
-tableHeader : Model -> Html Msg
-tableHeader model =
-    let
-        ids =
-            List.map .id model.contacts
-    in
-        thead []
-            [ headerInfoRow model
-            , tr []
-                [ th []
-                    [ input
-                        [ type_ "checkbox"
-                          --, onCheck (SetCheckbox ids)
-                        ]
-                        []
-                    ]
-                , th [] [ text "Last Name" ]
-                , th [] [ text "First Name" ]
-                , th [] [ text "Email" ]
-                ]
-            ]
-
-
-tableBody : List Contact -> Html Msg
-tableBody contacts =
-    tbody []
-        (contactRows contacts)
-
-
-contactsTable : Model -> Html Msg
-contactsTable model =
-    table [ class "table table-striped" ]
-        [ (tableHeader model)
-        , tableBody model.contacts
-        ]
-
-
-contactRows : List Contact -> List (Html Msg)
-contactRows contacts =
-    List.map
-        (\contact ->
-            tr []
-                [ td []
-                    [ input
-                        [ type_ "checkbox"
-                        , onCheck (SetCheckbox contact.id)
-                        ]
-                        []
-                    ]
-                , td [] [ text (Maybe.withDefault "" contact.lastName) ]
-                , td [] [ text (Maybe.withDefault "" contact.firstName) ]
-                , td [] [ text contact.email.address ]
-                ]
-        )
-        contacts
+import ContactsTable
 
 
 tagName : String -> List Tag -> String
@@ -164,7 +72,7 @@ mainContent model =
     div [ class "col-md-9" ]
         [ (errors model.httpError)
         , (contactsCount model)
-        , (contactsTable model)
+        , (ContactsTable.view model)
         , (setContactsPerPage model)
         , (pagination model)
         ]
